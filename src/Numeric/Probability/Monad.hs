d2fed4b3e951fe82dc369b5cb9b6d4afaeea3452
Mikko Juola <mikko.juola+dev@iki.fi>
2016-10-20 21:21:07 +0100

    Added cauchy distributions, removed toys.

--- src/Numeric/Probability/Monad.hs
+++ src/Numeric/Probability/Monad.hs
@@ -2,48 +2,123 @@
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE DeriveFoldable #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE MultiWayIf #-}
 
 module Numeric.Probability.Monad
-  ( happensProbability
+  ( pointDensityFunctionLB
+  , erf
+  , normalCdf
+  , cauchyCdf
   , Prob(..)
   , runProbabilityT
   , ProbF(..)
   , ProbabilityT
   , Happens(..)
   , bernoulli
+  , gaussian
+  , gaussianIntervalB
+  , gaussianInterval
+  , gaussianSmallerThanOther
+  , cauchy
+  , cauchyIntervalB
+  , cauchyInterval
+  , cauchySmallerThanOther
   , allHappen
   , atLeastOneHappens
   , bothHappen
   , eitherHappen
   , doesntHappen
-  , probability
-  , probabilityRaw )
+  , probability )
   where
 
 import Control.Monad.Free
 import Data.Data
-import Data.Foldable
-import Data.Traversable
 import GHC.Generics
 import Numeric.LogBase
 
 data Prob a
   = Constant a
   | Bernoulli a (Prob a) (Prob a)
+  | Cauchy a a a (Prob a)
+  | CauchyInterval a a a a (Prob a) (Prob a)
+  | Gaussian a a a (Prob a)
+  | GaussianInterval a a a a (Prob a) (Prob a)
   | DoesntHappen (Prob a)
   | BothHappen (Prob a) (Prob a)
   | EitherHappens (Prob a) (Prob a)
   deriving ( Eq, Ord, Show, Read, Typeable, Data, Generic, Functor, Foldable, Traversable )
 
-happensProbability :: Num a => Prob a -> a
-happensProbability (Constant prob) = prob
-happensProbability (Bernoulli prob p1 p2) =
-  (prob*happensProbability p1) +
-  ((1-prob)*happensProbability p2)
-happensProbability (BothHappen p1 p2) = happensProbability p1 * happensProbability p2
-happensProbability (EitherHappens p1 p2)
-  = 1 - ((1 - happensProbability p1) * (1 - happensProbability p2))
-happensProbability (DoesntHappen p1) = 1 - happensProbability p1
+erf :: Floating a => a -> a
+erf x = signum x * sqrt payload
+ where
+  x2 = x**2
+  a = (8*(pi - 3)) / (3 * pi * (4 - pi))
+  payload = 1 - exp (negate $ x2 * ((4/pi) + a*x2) / (1 + a*x2))
+{-# INLINE erf #-}
+
+normalCdf :: Floating a => a -> a -> a -> a
+normalCdf point mean sigma2 =
+  0.5 * (1 + erf ( (point-mean) / (sqrt sigma2 * sqrt 2) ) )
+{-# INLINE normalCdf #-}
+
+cauchyCdf :: Floating a => a -> a -> a -> a
+cauchyCdf point middle gamma =
+  (1/pi) * atan ( (middle-point)/gamma ) + 0.5
+{-# INLINE cauchyCdf #-}
+
+pointDensityFunctionLB :: (Ord a, RealFloat a) => Prob a -> LB a
+pointDensityFunctionLB (Constant prob) = toLogBase prob
+pointDensityFunctionLB (Bernoulli prob p1 p2) =
+  (toLogBase prob*pointDensityFunctionLB p1) +
+  ((toLogBase $ 1-prob)*pointDensityFunctionLB p2)
+pointDensityFunctionLB (BothHappen p1 p2) = pointDensityFunctionLB p1 * pointDensityFunctionLB p2
+pointDensityFunctionLB (EitherHappens p1 p2)
+  = 1 - ((1 - pointDensityFunctionLB p1) * (1 - pointDensityFunctionLB p2))
+pointDensityFunctionLB (DoesntHappen p1) = 1 - pointDensityFunctionLB p1
+
+pointDensityFunctionLB (GaussianInterval a b mean sigma2 p1 p2)
+  | b <= a = 0
+  | otherwise =
+      let a_cdf = normalCdf a mean sigma2
+          b_cdf = normalCdf b mean sigma2
+
+          positive_prob = b_cdf - a_cdf
+          negative_prob = 1.0 - positive_prob
+
+       in if | negative_prob == 1.0 && positive_prob == 0.0
+               -> pointDensityFunctionLB p2
+             | negative_prob == 0.0 && positive_prob == 1.0
+               -> pointDensityFunctionLB p1
+             | otherwise ->
+                (toLogBase positive_prob * pointDensityFunctionLB p1) +
+                (toLogBase negative_prob * pointDensityFunctionLB p2)
+
+pointDensityFunctionLB (Gaussian point mean sigma2 p1) =
+  let p = (1/(sqrt (2*sigma2*pi))) * exp (negate $ ((point - mean)**2)/(2 * sigma2))
+   in toLogBase p*pointDensityFunctionLB p1
+
+pointDensityFunctionLB (Cauchy point middle gamma p1) =
+  let p = (1/( (pi*gamma) * (1 + ((middle - point)/gamma)**2 ) ))
+   in toLogBase p*pointDensityFunctionLB p1
+
+pointDensityFunctionLB (CauchyInterval a b middle gamma p1 p2)
+  | b <= a = 0
+  | otherwise =
+      let a_cdf = cauchyCdf a middle gamma
+          b_cdf = cauchyCdf b middle gamma
+
+          positive_prob = b_cdf - a_cdf
+          negative_prob = 1.0 - positive_prob
+
+       in if | negative_prob == 1.0 && positive_prob == 0.0
+               -> pointDensityFunctionLB p2
+             | negative_prob == 0.0 && positive_prob == 1.0
+               -> pointDensityFunctionLB p1
+             | otherwise ->
+                (toLogBase positive_prob * pointDensityFunctionLB p1) +
+                (toLogBase negative_prob * pointDensityFunctionLB p2)
+
+{-# INLINE pointDensityFunctionLB #-}
 
 data ProbF f a
   = ConstantF f a
@@ -51,6 +126,10 @@ data ProbF f a
   | BernoulliF f a a
   | BothHappenF a a
   | EitherHappensF a a
+  | GaussianF f f f a
+  | GaussianIntervalF f f f f a a
+  | CauchyF f f f a
+  | CauchyIntervalF f f f f a a
   deriving ( Eq, Ord, Show, Read, Typeable, Data, Generic, Functor, Foldable, Traversable )
 
 type ProbabilityT f a = Free (ProbF f) a
@@ -80,7 +159,7 @@ bernoulliB :: f -> ProbabilityT f Happens -> ProbabilityT f Happens -> Probabili
 bernoulliB probability branch1 branch2 =
   Free $ BernoulliF probability branch1 branch2
 
-bernoulli :: Num f => f -> ProbabilityT f Happens
+bernoulli :: f -> ProbabilityT f Happens
 bernoulli probability = bernoulliB probability (return Happened) (return DidntHappen)
 
 doesntHappen :: ProbabilityT f Happens -> ProbabilityT f Happens
@@ -88,21 +167,56 @@ doesntHappen prob = Free $ DoesntHappenF prob
 
 probability :: RealFloat f => ProbabilityT f Happens -> LB f
 probability action =
-  let paction = fmap toLogBase $ runProbabilityT action
-   in happensProbability paction
+  let paction = runProbabilityT action
+   in pointDensityFunctionLB paction
 {-# INLINE probability #-}
 
-probabilityRaw :: Num f => ProbabilityT f Happens -> f
-probabilityRaw action = happensProbability $ runProbabilityT action
-{-# INLINE probabilityRaw #-}
+gaussian :: f -> f -> f -> ProbabilityT f Happens
+gaussian point mean sigma2 = Free $ GaussianF point mean sigma2 (return Happened)
+{-# INLINE gaussian #-}
+
+gaussianIntervalB :: f -> f -> f -> f -> ProbabilityT f Happens -> ProbabilityT f Happens -> ProbabilityT f Happens
+gaussianIntervalB a b mean sigma2 p1 p2 = Free $ GaussianIntervalF a b mean sigma2 p1 p2
+{-# INLINE gaussianIntervalB #-}
+
+gaussianInterval :: f -> f -> f -> f -> ProbabilityT f Happens
+gaussianInterval a b mean sigma2 = gaussianIntervalB a b mean sigma2 (return Happened) (return DidntHappen)
+{-# INLINE gaussianInterval #-}
+
+gaussianSmallerThanOther :: Num f => f -> f -> f -> f -> ProbabilityT f Happens
+gaussianSmallerThanOther mean1 sigma12 mean2 sigma22 =
+  gaussianInterval (negate (sigma12+sigma22) * 1000) 0 (mean1-mean2) (sigma12+sigma22)
+{-# INLINE gaussianSmallerThanOther #-}
+
+cauchy :: f -> f -> f -> ProbabilityT f Happens
+cauchy point middle gamma = Free $ CauchyF point middle gamma (return Happened)
+{-# INLINE cauchy #-}
+
+cauchyIntervalB :: f -> f -> f -> f -> ProbabilityT f Happens -> ProbabilityT f Happens -> ProbabilityT f Happens
+cauchyIntervalB a b middle gamma p1 p2 = Free $ GaussianIntervalF a b middle gamma p1 p2
+{-# INLINE cauchyIntervalB #-}
+
+cauchyInterval :: f -> f -> f -> f -> ProbabilityT f Happens
+cauchyInterval a b middle gamma = cauchyIntervalB a b middle gamma (return Happened) (return DidntHappen)
+{-# INLINE cauchyInterval #-}
+
+cauchySmallerThanOther :: Num f => f -> f -> f -> f -> ProbabilityT f Happens
+cauchySmallerThanOther middle1 gamma1 middle2 gamma2 =
+  cauchyInterval (negate (gamma1+gamma2) * 1000000) 0 (middle1-middle2) (gamma1+gamma2)
+{-# INLINE cauchySmallerThanOther #-}
 
 runProbabilityT :: Num f => ProbabilityT f Happens -> Prob f
 runProbabilityT (Pure Happened) = Constant 1
 runProbabilityT (Pure DidntHappen) = Constant 0
 runProbabilityT (Free inside) = case inside of
-  ConstantF v next -> runProbabilityT next
+  ConstantF _v next -> runProbabilityT next
   BernoulliF v b1 b2 -> Bernoulli v (runProbabilityT b1) (runProbabilityT b2)
   BothHappenF b1 b2 -> BothHappen (runProbabilityT b1) (runProbabilityT b2)
   DoesntHappenF b1 -> DoesntHappen (runProbabilityT b1)
   EitherHappensF b1 b2 -> EitherHappens (runProbabilityT b1) (runProbabilityT b2)
+  GaussianF point mean sigma2 next -> Gaussian point mean sigma2 (runProbabilityT next)
+  GaussianIntervalF a b mean sigma2 p1 p2 -> GaussianInterval a b mean sigma2 (runProbabilityT p1) (runProbabilityT p2)
+  CauchyF point middle gamma next -> Cauchy point middle gamma (runProbabilityT next)
+  CauchyIntervalF a b middle gamma p1 p2 -> CauchyInterval a b middle gamma (runProbabilityT p1) (runProbabilityT p2)
+{-# INLINE runProbabilityT #-}
 
